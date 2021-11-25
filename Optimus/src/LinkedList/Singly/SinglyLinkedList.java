a82468966f0109108a0f03406259ae415dd2059d
SagarKumar04 <sagarsngh93@gmail.com>
2021-11-25 23:07:59 +0530

    Abstract Classes, Interface, Singly Linked List

---
+++ Optimus/src/LinkedList/Singly/SinglyLinkedList.java
@@ -0,0 +1,19 @@
+package LinkedList.Singly;
+
+public class SinglyLinkedList {
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
